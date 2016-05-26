6568ca4512562f13e2ff4c611b36aa814e7b6209
James Koppel <jamesbkoppel@yahoo.com>
2016-05-25 19:56:02 -0500

    Checkpoint on merging in Chrisbigart's work on CD music. Not working yet

--- ironfist/src/cpp/sound/sound.h
+++ ironfist/src/cpp/sound/sound.h
@@ -1,17 +1,58 @@
 #ifndef SOUND_H
 #define SOUND_H
 
+#include "manager.h"
 
-class soundManager {
+#pragma pack(push, 1)
+
+// This is defined in MSS, but MSS is mutually exclusive with BASS; they use some of the same symbol names
+typedef void* HDIGDRIVER;
+
+class soundManager : public baseManager {
 public:
-	char _[1714];
-	soundManager();
+  int redbookStatus;
+  HDIGDRIVER hdidriver;
+  int field_3E;
+  char _0[22];
+  void * hsamples[14];  //HSAMPLE hsamples[14];
+  int field_90;
+  int field_94;
+  int numSamplesAllocated;
+  char _1[84];
+  char _2[1152];
+  int field_570;
+  int _3;
+  int field_578;
+  char currentTrack;
+  char _4[267];
+  int aiMusicOff;
+  int volRelated;
+  int _5;
+  int field_694;
+  int field_698;
+  __int16 outputDeviceID;
+  int field_69E;
+  int usingRedbookRelated;
+  int field_6A6;
+  float field_6AA;
+  int someTick;
+  soundManager();
 
-	void SwitchAmbientMusic(int track);
+  void SwitchAmbientMusic(int track);
+  void CDPlay(int track_number, signed int a3, int a4, int a5);
+  void CDStop();
+  void CDSetVolume(int, int);
+  void CDSetVolume_orig(int, int);
+  int ConvertVolume(int, int);
+  int ConvertVolume_orig(int, int);
+  void ServiceSound();
+  void CDStartup();
 };
 
 extern soundManager *gpSoundManager;
 
 extern unsigned char giTerrainToMusicTrack[];
+
+#pragma pack(pop)
 
 #endif
